160744e4b95e383dd5bb75832bff5ced460c730e
jhennen <jhennen>
2017-03-30 14:08:48 -0500

    Initial file load

---
+++ README.md
@@ -0,0 +1,6 @@
+## Introduction
+
+This is the Git repository for the Node.js MySQL Microservice module for the **Oracle Public Cloud DevOps Cloud Native Microservices-DB Workshop.** There are no dependencies for this Node.js module.  However, a standard MySQL subdirectory is included in this code.  The main js file for this Node.js module is server.js.  This module performs three very simple functions:
+- First server.js calls the MySQL database and selects data on products and categories.
+- Secondly the server.js code then formats this result set into JSON.
+- Lastly server.js writes this JSON to the web.
---
+++ server.js
@@ -0,0 +1,73 @@
+var http = require("http");
+var mysql = require("./mysql");
+
+var resultsArray;
+var tab1 = "   ";
+var tab2 = "      ";
+var tab3 = "         ";
+var tab4 = "            ";
+var tab5 = "               ";
+
+// USE THE MYSQL NODE.JS CODE LOCATED IN THE MYSQL SUBFOLDER TO CREATE A CONNECTION OBJECT WITH APPROPRIATE VALUES FOR CONNECTING TO THE MYSQL DB IN THE ORACLE CLOUD SERVICE.
+var connection = mysql.createConnection({
+    host     : '141.145.24.235',
+    port     : '1521',
+    user     : 'root',
+    password : 'flUEnt@0Fate',
+    database : 'AlphaofficeDB'
+});
+
+// PERFORM A QUERY USING THE SYNTAX IN THE SQLVAR VARIABLE.  THE RESULTING ROWS WILL BE WRITTEN TO THE RESULTSARRAY ARRAY.
+connection.connect();
+var sqlVar = "SELECT c.category_name, p.product_name, p.twitter_tag FROM `PRODUCTS` AS p INNER JOIN `PRODUCT_CATEGORIES` AS c ON c.category_id = p.category_id ORDER BY c.category_name, p.product_name";
+connection.query(sqlVar, function (error, results, fields) {
+    if (error){
+        console.log(error);
+    }
+    resultsArray = results;
+});
+connection.end();
+
+// GENERATE AND RETURN A JSON FILE USING ROW VALUES SELECTED FROM THE DATABASE.
+http.createServer(function(request, response) {
+response.writeHead(200, {
+    'Access-Control-Allow-Origin' : '*',
+    'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE',
+    'Content-Type': 'application/json'
+});
+    // GENERATE RETURN JSON OBJECT.
+    response.write("{\r\n");
+    response.write(tab1 + "\"products\":[\r\n");
+    /*********************************************************    
+    UNCOMMENT THE CODE SECTION BELOW FOR THE WORKSHOP - THIS SECTION ASSEMBLES AND RETURNS THE DATABASE PRODUCT DATA.      
+    *********************************************************/  
+      
+    for (var i in resultsArray) {
+        if (i > 0) {
+            response.write(tab2 + "},\r\n");          
+        }
+        response.write(tab2 + "{\r\n");  
+        response.write(tab3 + "\"category\":\"" + cleanseValues(resultsArray[i].category_name)   + "\",\r\n");  
+        response.write(tab3 + "\"productName\":\"" + cleanseValues(resultsArray[i].product_name)  + "\",\r\n");  
+        response.write(tab3 + "\"twitterTag\":\"" + cleanseValues(resultsArray[i].twitter_tag)  + "\"\r\n");    
+    }
+    response.write(tab2 + "}\r\n");  
+    response.write(tab1 + "]\r\n"); 
+    response.write("}\r\n");  
+
+    /*********************************************************    
+    UNCOMMENT THE CODE SECTION ABOVE FOR THE WORKSHOP.    
+    *********************************************************/    
+    response.end();
+}).listen(8002);
+
+// REMOVE ANY NON-DISPLAYABLE CHARACTERS.
+function cleanseValues(textParm) {
+    var textVar = textParm;
+    textVar = textVar.replace(/"/g, '\"');
+    textVar = textVar.replace(/[^A-Za-z 0-9 \.,\?""!@#\$%\^&\*\(\)-_=\+;:<>\/\\\|\}\{\[\]`~]*/g, '')
+    return textVar;
+}
+
+
+ 
